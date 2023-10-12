957b2003673446edcc5fe7109c4efc59bec40e67
Mmulobi <mosesmutush7@gmail.com>
2023-10-12 23:43:50 +0000

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,9 @@
+def minOperations(n):
+    num_ops = 0
+    min_ops = 2
+    while n > 1:
+        while n % min_ops == 0:
+            num_ops += min_ops
+            n /= min_ops
+        min_ops += 1
+    return num_ops
